4fb0eba82643f75f9e1aab9ce0b3de988b0f50ea
Thalentesithole <3944942@myuwc.ac.za>
2026-04-22 02:15:31 +0200

    folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+#Creating directory
+mkdir -p src
+mkdir -p research
+
+#Creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch main.py
+touch research/trials.ipynb
+touch README.md
+touch requirements.txt
+
+
+echo "Directory and files have been created successfully!."
