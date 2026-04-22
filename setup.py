6f23f82865ad9270b40d7374c2f26f8d9bfc9b34
Thalentesithole <3944942@myuwc.ac.za>
2026-04-22 03:33:29 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="learners_license_lessons_chatbot",
+    version="0.1.0",
+    author="Thalente Sithole",
+    author_email="Thalentesithole1@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
